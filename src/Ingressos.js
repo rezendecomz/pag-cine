4ed74d934a1904667285cc1edcf4990991e8d6ad
Gabriel Rezende <52981329+rezendecomz@users.noreply.github.com>
2021-02-22 18:41:42 -0300

    Revert "criado a tela de selecao de poltrona"

--- src/Ingressos.js
+++ src/Ingressos.js
@@ -1,95 +1,12 @@
 const Ingressos = () => {
   return ( 
-
     <div className="container">
-    <h1>Reserva de assentos</h1>          
-    {/* <div className="ref d-flex justify-content-center">             */}
-    <div class="disponivel">Disponível</div>
-     <div class="ocupada">Ocupada</div>
-    {/* </div>     */}
-     {/* <h1>Selecione</h1>                */}
-     <br></br>
-     
-    </div> 
+    <div className="ref d-flex justify-content-center">
+    <img src="/img/ref/ref_checkout.png" alt="" srcset="" width="130%"/>
+    </div>
 
-  );
-} 
-
-// Gera um array de reservas de poltonas
-var reservas = [], qtdePoltronas = 35;
-
-for (let i = 0; i < qtdePoltronas; i++) {
-  reservas[i] = false;    
+    </div>
+   );
 }
-
-let localOcupado = 0;
-
-const fnIniciarSessao = (array) => {
-    const containerSeats = document.getElementById("poltronas")
-
-    for (const i in array) {
-         let setPoltrona = document.createElement("p");
-         setPoltrona.className = "poltronas";            
-         setPoltrona.style.backgroundColor = "green";                          
-         setPoltrona.innerHTML =`<h1>${parseInt(i) + 1}</h1>`;
-                                
-                
-                containerSeats.appendChild(setPoltrona);                         
-        }           
-    }
-
-    fnIniciarSessao(reservas);
-
-    const fnReservarPoltrona = () => {       
-        
-      const reserverBtn = document.getElementById("poltronas") ;
-             reserverBtn.addEventListener("click",marcar);       
-   }    
-
-   const fnOcuparLugar = (i) => {
-       const setPoltrona = document.querySelectorAll(".poltronas");        
-       setPoltrona[i].style.backgroundColor = "red";       
-   }      
-
-   const marcar = () => {
-     if (localOcupado === qtdePoltronas) {
-      fnSessaoLotada();                           
-     } else { 
-           for (let i = 0; i < reservas.length; i++) {
-               if (reservas[i] === false) {
-                   reservas[i] = true;
-                   fnOcuparLugar(i);
-                   localOcupado++;
-                   let qtdeOcupada = i + 1;
-                   fnResumoContagem(qtdeOcupada);
-                   
-                   break;
-               }
-          }
-       }
-   }        
-
-const  fnSessaoLotada = () => {
-alert("Atenção: Sessão lotada!");
-}
-// var x = '5*-'
-const fnResumoContagem = (qtdeOcupada) => {
-   const ticket = document.getElementById("ingresso");
-   ticket.innerHTML = `<div id="tela">
-                       Tela do Cinema
- </div>
-    <div class="root">                            
-    <p>Qtde Ocupada: ${qtdeOcupada}</p>                           
-    <p>Qtde Disponível: ${qtdePoltronas-qtdeOcupada}</p>
-  </div>`                        
-}
-
-
-fnReservarPoltrona();
-
+ 
 export default Ingressos;
-
-
-
-
-

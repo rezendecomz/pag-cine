e428ee525b831dfa5e99501b952c45cfe8ac3cfe
Gabriel Rezende <g@brielrezen.de>
2021-04-20 21:27:27 -0300

    App.css removido e nova indentação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+// import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
 import './css/bootstrap.min.css'
 import './css/style.css'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ListaDeFilmes from './EmCartaz';
 import Login from './Login'
 import Registro from './Registro'
@@ -18,44 +18,44 @@ function App() {
 
   return (
     <Router>
-<div>
+      <div>
 
-  <div className="container">
-  <Navbar />
-  <Switch>
-    <Route exact path="/">
-      <Home/>
-    </Route>
-    <Route exact path="/em-cartaz">
-        <ListaDeFilmes/>
-    </Route>
-    <Route exact path="/lista-de-filmes">
-        <ListaDeFilmes/>
-    </Route>
-    <Route exact path="/trailers/54321">
-        <Trailer/>
-    </Route>
-    <Route exact path="/ingressos/54321">
-        <Cinema chairs={chairs} />        
-    </Route>
-    <Route exact path="/programacao">
-        <Programacao/>
-    </Route>
-    <Route exact path="/login">
-      <Login/>
-    </Route>
-    <Route exact path="/minhaConta">
-      <MinhaConta/>
-    </Route>
-    <Route exact path="/registro">
-      <Registro/>
-    </Route>
-    <Route exact path="/esqueci">
-      <Esqueci/>
-    </Route>
-  </Switch>
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/em-cartaz">
+              <ListaDeFilmes />
+            </Route>
+            <Route exact path="/lista-de-filmes">
+              <ListaDeFilmes />
+            </Route>
+            <Route exact path="/trailers/54321">
+              <Trailer />
+            </Route>
+            <Route exact path="/ingressos/54321">
+              <Cinema chairs={chairs} />
+            </Route>
+            <Route exact path="/programacao">
+              <Programacao />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/minhaConta">
+              <MinhaConta />
+            </Route>
+            <Route exact path="/registro">
+              <Registro />
+            </Route>
+            <Route exact path="/esqueci">
+              <Esqueci />
+            </Route>
+          </Switch>
+        </div>
       </div>
-</div>
     </Router>
   );
 }

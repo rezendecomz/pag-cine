5269bb1817b9069c6540076053586da9ea4c24a2
ams-mari <ams_mariana@outlook.com>
2021-04-23 20:03:44 -0300

    adicionando método map em carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,34 +1,48 @@
 import listaCarrousel from './filmes/listaCarrousel'
 
-// Alguma coisa no js do bootstrap impede essa função e o 
+// Alguma coisa no js do bootstrap impede essa função e o
 // const ehActive = (prop) => {
-//   if (prop['id'] == 1) {
-//     return 'active'
-//   } else {
-//     return ''
-//   }
+// if (prop['id'] == 1) {
+// return 'active'
+// } else {
+// return ''
+// }
 // }
 
 const Carrousel = () => {
   return (
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
       <div className="carousel-inner">
-      
-      {/* { listaCarrousel.map( filme => (
+
+        {/* { listaCarrousel.map( filme => (
         <div className={`carousel-item ${ehActive(filme)}`}>
           <img src={ filme['img'] } className="d-block w-100" className="nada" alt={ filme['alt']} />
         </div>
-      )) } */}
-        <div className={`carousel-item active`}>
-          <img src={ listaCarrousel[0]['img'] } className="d-block w-100"  alt={ listaCarrousel[0]['alt']} />
+        )) } */}
+
+        {
+          listaCarrousel.map((filme,index)=>{
+            return index==0 ? (
+              <div className={`carousel-item active`} key={filme.id}>
+                <img src={filme.img} className="d-block w-100" alt={ filme.alt} />
+              </div>
+            ) : ( 
+              <div className="carousel-item" key={filme.id}>
+                <img src={filme.img} className="d-block w-100" alt={ filme.alt} />
+              </div> 
+            )
+          })
+        }
+
+        {/* <div className={`carousel-item active`}>
+          <img src={ listaCarrousel[0]['img'] } className="d-block w-100" alt={ listaCarrousel[0]['alt']} />
         </div>
-        <div className="carousel-item">
-          <img src={ listaCarrousel[1]['img'] } className="d-block w-100" alt={ listaCarrousel[1]['alt']} />
-        </div>
+        
         <div className="carousel-item">
           <img src={ listaCarrousel[2]['img'] } className="d-block w-100" alt={ listaCarrousel[2]['alt']} />
-        </div>
-      </div>
+        </div>*/}
+        
+      </div> 
       <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true" />
         <span className="sr-only">Previous</span>
@@ -40,5 +54,5 @@ const Carrousel = () => {
     </div>
   );
 }
- 
+
 export default Carrousel;

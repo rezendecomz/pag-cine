993aed50c97b5c67dd79913ba7a0e00c2da8e44a
Gabriel Rezende <g@brielrezen.de>
2021-04-24 20:46:31 -0300

    página de trailer componentizada

---
+++ src/components/TrailerComp.js
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+const TrailerComp = ({ filme }) => {
+  return (
+    <>
+      <div className="card mt-3 p-3">
+        <h1>{filme.titulo}</h1>
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src={"https://www.youtube.com/embed/" + filme.trailer_url + "?rel=0"} allowfullscreen></iframe>
+        </div>
+
+        <div class="card mt-3">
+          <div className="row no-gutters mb-2">
+            <div className="col-auto mb-2">
+              <img src={filme.poster_url} className="img-fluid" alt="" />
+            </div>
+            <div className="col-4 " style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
+              <div className="card-block px-2 d-flex align-items-start flex-column">
+
+                <h4 className="card-title mt-2">{filme.titulo}</h4>
+                <p>Cinema mais próximo:</p>
+                <p className="card-text"><i className="fas fa-map-marker-alt fa-lg" />Cine Belas Artes, São Paulo</p>
+                <p>Próxima sessão:</p>
+                <p className="card-text"><i class="fas fa-calendar-alt fa-lg"></i>22/02/2021 - 19h30</p>
+                <p className="card-text"><i class="fas fa-video fa-lg"></i>Comum, IMAX, 3D</p>
+                <p>Ingressos a partir de:</p>
+                <p className="card-text"><i class="fas fa-dollar-sign fa-lg"></i> Total: R$ 27,50</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card-block px-2 d-flex align-items-start flex-column">
+                <h4 className="card-title mt-2">Sinopse</h4>
+                <p className="card-text">{filme.['sinopse']}</p>
+                <p className="card-text"><i class="far fa-id-card fa-lg"></i>   {filme.classificacao}</p>
+                <Link to={"/ingressos/" + filme.id} className="btn btn-primary mt-3">Ingressos</Link>
+
+
+              </div>
+            </div>
+            <div className="col-auto">
+              <img src="./img/qrExample.png" alt="" srcset="" />
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  );
+}
+
+export default TrailerComp;
--- src/filmes/trailer.js
+++ src/filmes/trailer.js
@@ -1,55 +1,46 @@
-import { Link } from "react-router-dom";
-import filmes from './listaFilmes';
+// import { Link } from "react-router-dom";
+import { useState, useEffect } from "react"
+import TrailerComp from '../components/TrailerComp'
+// import filmes from './listaFilmes';
 
 const Trailer = () => {
-  return ( 
-    <div className="container">
-    <div className="card mt-3 p-3">
-          <h1>{filmes[0]['nome']}</h1>
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XqcsS1azJ_g?rel=0" allowfullscreen></iframe>
-      </div>
+    const [filme, setFilme] = useState(null)
 
-      <div class="card mt-3">
-                <div className="row no-gutters mb-2">
-            <div className="col-auto mb-2">
-                <img src={filmes[0]['img']} className="img-fluid" alt=""/>
-            </div>
-            <div className="col-4 " style={{backgroundColor:'rgba(0, 0, 0, 0.1)'}}>
-                <div className="card-block px-2 d-flex align-items-start flex-column">
+    const [isPending, setIsPending] = useState(true)
 
-                    <h4 className="card-title mt-2">{filmes[0]['nome']}</h4>
-                    <p>Cinema mais próximo:</p>
-                    <p className="card-text"><i className="fas fa-map-marker-alt fa-lg" />Cine Belas Artes, São Paulo</p>
-                    <p>Próxima sessão:</p>
-                    <p className="card-text"><i class="fas fa-calendar-alt fa-lg"></i>22/02/2021 - 19h30</p>
-                    <p className="card-text"><i class="fas fa-video fa-lg"></i>Comum, IMAX, 3D</p>
-                    <p>Ingressos a partir de:</p>
-                    <p className="card-text"><i class="fas fa-dollar-sign fa-lg"></i> Total: R$ 27,50</p>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card-block px-2 d-flex align-items-start flex-column">
-                    <h4 className="card-title mt-2">Sinopse</h4>
-                    <p className="card-text">{filmes[0]['sinopse']}</p>
-                    <p className="card-text"><i class="far fa-id-card fa-lg"></i>   {filmes[0]['classificacao']}</p>
-      <Link to="/ingressos/54321" className="btn btn-primary mt-3">Ingressos</Link>
+    const [error, setError] = useState(null);
 
+    useEffect(() => {
+        fetch('http://localhost:3333/filme/1')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Problema com conexão com o servidor'); // mensagem do erro
+                }
+                return res.json()
+            })
+            .then((data) => {
+                // console.log(data)
+                setFilme(data)
+                setIsPending(false)
+            })
+            .catch(err => {
+                setIsPending(true)
+                setError(err.message)
+                // setError(null)
+            })
+    }, [])
 
-                </div>
-            </div>
-            <div className="col-auto">
-              <img src="./img/qrExample.png" alt="" srcset=""/>
-            </div>
+    return (
+        <div className="container">
+            {/* {console.log(filmes)} */}
+            {error && <div>{error}</div>}
+            {isPending && <div>Carregando...</div>}
+            {!isPending && <TrailerComp filme={filme} />}
+            {/* <h1>A</h1>
+            <TrailerComp filmes={filmes} /> */}
         </div>
-    </div>
 
-
-    </div>
-
-    </div>
-
-   );
+    );
 }
- 
+
 export default Trailer;

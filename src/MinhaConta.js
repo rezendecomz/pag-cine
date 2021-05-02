79d1e2464f75d36680fb2535a8020616b3237fd9
Gabriel Rezende <g@brielrezen.de>
2021-05-02 12:38:17 -0300

    atualização visual

--- src/MinhaConta.js
+++ src/MinhaConta.js
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react';
 import User from './status';
 import filmes from './filmes/listaFilmes'
 
-const MinhaConta = () => {
+const MinhaConta = ({ativo}) => {
  
   return ( 
     <div>
     {useEffect(() => {
-      document.getElementById('login').innerText = 'Bem-vindo, Fulano';
+      document.getElementById('login').innerText = 'João Pedro';
+      document.getElementById('loginHref').href = "/minhaConta2"
     })}
 <div className="container mt-2">
   <div className="row">
     <div className="col d-flex justify-content-center align-items-center" style={{backgroundColor: 'coral', height: '50px'}}>
-      <h5 style={{color: 'white'}}>Olá, Fulano.
+      <h5 style={{color: 'white'}}>Olá, João Pedro.
         Bem-vindo à sua conta :)</h5>
     </div>
   </div>
@@ -25,6 +26,39 @@ const MinhaConta = () => {
 
     
     <div className='listaDeFilme mt-4'>
+    {ativo &&
+    <div className="row no-gutters">
+            <div className="col-auto">
+                <img src={'/img/posters/minari.jpg'} className="img-fluid" alt=""/>
+            </div>
+            <div className="col ml-2 mb-2" style={{backgroundColor:'rgba(0, 0, 0, 0.1)'}}>
+                <div className="card-block px-2 d-flex align-items-start flex-column">
+
+                    <h4 className="card-title mt-2">Minari</h4>
+                    <p className="card-text"><i className="fas fa-map-marker-alt fa-lg" />Cine Belas Artes, São Paulo</p>
+                    <p className="card-text"><i class="fas fa-video fa-lg"></i>Sala 2</p>
+                    <p className="card-text"><i class="fas fa-calendar-alt fa-lg"></i>22/02/2021 - 19h30</p>
+                    <p className="card-text"><i class="fas fa-couch fa-lg"></i> E 4, E 5, E 6</p>
+                    <p className="card-text"><i class="far fa-id-card fa-lg"></i>  10 anos</p>
+                    <p className="card-text"><i class="fas fa-language fa-lg"></i> Inglês (legendado)</p>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card-block px-2 d-flex align-items-start flex-column">
+                    <h4 className="card-title mt-2">Dados do pagamento:</h4>
+                    <p className="card-text"><i class="fas fa-calendar-alt fa-lg"></i>03/05/2021</p>
+                    <p className="card-text"><i class="fas fa-credit-card fa-lg"></i>Crédito -  MasterCard</p>
+                    <p className="card-text"><i class="fas fa-dollar-sign fa-lg"></i> Total: R$ 82,50</p>
+                    <p className="card-text"><i class="far fa-sticky-note fa-lg"></i>  Pedido: 8954645</p>
+
+                </div>
+            </div>
+            <div className="col-auto">
+              <img src="./img/qrExample.png" alt="" srcset=""/>
+            </div>
+        </div>
+    }
+    </div>
       {filmes.map( filme => (
 <div class="card mt-3">
                 <div className="row no-gutters">
@@ -65,7 +99,6 @@ const MinhaConta = () => {
     </div>
 
 </div>
-    </div>
    );
 }
  
